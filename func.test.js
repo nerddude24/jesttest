87c52370ef0406cf6bf1845271178f7af1cd33ff
nerddude <30808467+nerddude24@users.noreply.github.com>
2024-09-05 09:14:25 +0100

    feat: add capitalize func with tests

--- func.test.js
+++ func.test.js
@@ -1,3 +1,11 @@
-const funcs = require("./func");
+const funcs = require("./func.js");
 
-test("func object exists", () => expect(funcs).not.toBeUndefined());
+test("func object exists", () => expect(funcs).not.toBeNull());
+
+describe("Capitalization", () => {
+	test("normal", () => expect(funcs.capitalize("hi")).toEqual("Hi"));
+	test("all caps", () => expect(funcs.capitalize("HI")).toEqual("HI"));
+	test("with numbers and symbols", () =>
+		expect(funcs.capitalize("yo!1!")).toEqual("Yo!1!"));
+	test("only symbols", () => expect(funcs.capitalize("!!!")).toEqual("!!!"));
+});
