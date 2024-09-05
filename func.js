784b8152ef4bc57d4922752058b5c8726d4033bd
nerddude <30808467+nerddude24@users.noreply.github.com>
2024-09-05 09:19:19 +0100

    feat: reverseString func with tests

--- func.js
+++ func.js
@@ -2,4 +2,10 @@ function capitalize(str) {
 	return str[0].toUpperCase() + str.slice(1);
 }
 
-module.exports = { capitalize };
+function reverseString(str) {
+	let rev = "";
+	for (let i = str.length - 1; i >= 0; i--) rev += str[i];
+	return rev;
+}
+
+module.exports = { capitalize, reverseString };
--- func.test.js
+++ func.test.js
@@ -3,9 +3,16 @@ const funcs = require("./func.js");
 test("func object exists", () => expect(funcs).not.toBeNull());
 
 describe("Capitalization", () => {
-	test("normal", () => expect(funcs.capitalize("hi")).toEqual("Hi"));
-	test("all caps", () => expect(funcs.capitalize("HI")).toEqual("HI"));
+	test("letters only", () => expect(funcs.capitalize("hi")).toEqual("Hi"));
+	test("letters all caps", () => expect(funcs.capitalize("HI")).toEqual("HI"));
 	test("with numbers and symbols", () =>
 		expect(funcs.capitalize("yo!1!")).toEqual("Yo!1!"));
 	test("only symbols", () => expect(funcs.capitalize("!!!")).toEqual("!!!"));
+});
+
+describe("String Reversion", () => {
+	test("letters only", () =>
+		expect(funcs.reverseString("wadup")).toEqual("pudaw"));
+	test("letters and symbols", () =>
+		expect(funcs.reverseString("wadup!!1")).toEqual("1!!pudaw"));
 });
